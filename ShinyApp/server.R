4991b3526f27535bd0c39aedaeb0fdf03b58292c
David <98davidklee@gmail.com>
2018-03-02 19:40:02 -0800

    Done

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -1,11 +1,7 @@
 library(shiny)
 
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$value <- renderPrint({ 
-    if(input$dates == "2007-01-03")  {
-    }
-  })
   
 })
