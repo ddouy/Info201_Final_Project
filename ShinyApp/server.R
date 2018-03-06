a50feaf16643a93a84fdd7093fc2e84b86771995
Sangho Bak <sanghobak97@gmail.com>
2018-03-05 18:33:59 -0800

    Made two widgets

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -4,4 +4,11 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+  
+  
+  
+  output$industry <- renderPrint({ input$industry })
+  output$name <- renderPrint({ input$names})
+  
+  
 })
--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -1,5 +1,15 @@
 
 library(shiny)
+library(dplyr)
+library(xml2)
+library(rvest)
+
+tbl <- read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies') %>% html_nodes(css = 'table')
+tbl <- tbl[1] %>% html_table() %>% as.data.frame()
+tbl$Ticker.symbol <- gsub(pattern = '\\.', '-', tbl$Ticker.symbol)
+SM500security <- tbl$Security
+SM500symbol <- tbl$Ticker.symbol
+SM500Industries <- unique(tbl$GICS.Sector)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -12,12 +22,21 @@ shinyUI(fluidPage(
     sidebarPanel(
       dateRangeInput("dates", label = "Date range",
                      start = "2007-01-03", end = "2018-03-01",
-                     min = "2007-01-03", max = "2018-03-01")
-    ),
+                     min = "2007-01-03", max = "2018-03-01"),
     
+    
+    selectInput("industry", label = h3("Industry"), 
+                choices = SM500Industries, 
+                selected = 1),
+    
+    selectInput("names", label = h3("Company Abbreviation"), 
+                choices = SM500symbol, 
+                selected = 1)
+    
+  ),
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+      plotOutput("distPlot")
     )
   )
 ))
